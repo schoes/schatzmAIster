a38f864da38d067b6ddbedb1ee64b38a0ed1aeee
Sven Schoeni <sven.schoeni@gmail.com>
2018-05-28 14:40:52 +0200

    Initial Commit

---
+++ SchatzmAIster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SchatzmAIster
+//
+//  Created by Sven Schoeni on 28.05.18.
+//  Copyright © 2018 Sven Schoeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
