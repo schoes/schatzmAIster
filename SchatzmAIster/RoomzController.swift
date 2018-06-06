96fa08513a976e9bc7e3e0c01fe21d2a8d678110
Jan Scheidegger <scheideggerj@gmail.com>
2018-06-06 11:00:22 +0200

    Added Camera Function

--- SchatzmAIster/RoomzController.swift
+++ SchatzmAIster/RoomzController.swift
@@ -8,7 +8,62 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+
+var captureSession: AVCaptureSession?
+var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
 class RoomzController:UIViewController {
+    
+    @IBOutlet var previewLayer: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+            fatalError("No video device found")
+        }
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous deivce object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object
+            captureSession = AVCaptureSession()
+            
+            // Set the input devcie on the capture session
+            captureSession?.addInput(input)
+            
+            
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the input device
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            
+            
+            
+            //Initialise the video preview layer and add it as a sublayer to the viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewLayer.layer.addSublayer(videoPreviewLayer!)
+            
+            //start video capture
+            captureSession?.startRunning()
+        
+        
+        } catch {
+            //If any error occurs, simply print it out
+            print(error)
+            return
+        }
+        
+        
+    }
     
 }
