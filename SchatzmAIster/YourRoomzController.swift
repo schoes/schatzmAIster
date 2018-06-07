8d15f9868b43d77ba8377ea93f9da7842d46f705
Sven Schoeni <sven.schoeni@gmail.com>
2018-06-07 11:27:04 +0200

    ui improvements

---
+++ SchatzmAIster/YourRoomzController.swift
@@ -0,0 +1,15 @@
+//
+//  YourRoomzController.swift
+//  SchatzmAIster
+//
+//  Created by Sven Schoeni on 07.06.18.
+//  Copyright © 2018 Sven Schoeni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class YourRoomzController:UITableViewController{
+    
+}
