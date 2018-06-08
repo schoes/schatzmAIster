0ca1e3a2732750b5941fbe66396227e70d805f5b
Jan Scheidegger <scheideggerj@gmail.com>
2018-06-08 08:21:41 +0200

    - IntroViewController added

---
+++ SchatzmAIster/IntroViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroViewController.swift
+//  SchatzmAIster
+//
+//  Created by Jan Scheidegger on 08.06.18.
+//  Copyright © 2018 Sven Schoeni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IntroViewController:UIViewController{
+    
+}
